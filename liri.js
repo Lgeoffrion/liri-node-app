78e0d9b62aae225b2464fa5b5a5d67b57e524d26
Logan Geoffrion <45701086+Lgeoffrion@users.noreply.github.com>
2019-02-21 22:11:52 -0600

    final coding complete and readme updated

--- liri.js
+++ liri.js
@@ -10,15 +10,14 @@ var spotify = new Spotify(keys.spotify);
 
 // Operators for if functions
 let operator = process.argv[2];
-let operatorConcert = process.argv.slice(3).join("");
-let operatorOMDB = process.argv.slice(3).join("+");
+let operator2 = process.argv.slice(3).join("+"); //this might be the only one i need
 let artistName = process.argv.slice(3).join(" ");
 
 // * `concert-this`
 // `node liri.js concert-this <artist/band name here>`
 if (operator === "concert-this") {
     axios
-        .get("https://rest.bandsintown.com/artists/" + operatorConcert + "/events?app_id=codingbootcamp")
+        .get("https://rest.bandsintown.com/artists/" + operator2 + "/events?app_id=codingbootcamp")
         .then(function (response) {
             eventData = response.data;
             for (var i = 0; i < eventData.length; i++) {
@@ -26,6 +25,14 @@ if (operator === "concert-this") {
                     "\nVenue Name: " + eventData[i].venue.name +
                     "\nVenue Location: " + eventData[i].venue.city + " " + eventData[i].venue.region + " " + eventData[i].venue.country +
                     "\nDate/Time: " + moment(eventData[i].datetime).format('MMMM Do YYYY, h:mm a'));
+                fs.appendFile("log.txt",
+                    "\nBand/Artist: " + artistName +
+                    "\nVenue Name: " + eventData[i].venue.name +
+                    "\nVenue Location: " + eventData[i].venue.city + " " + eventData[i].venue.region + " " + eventData[i].venue.country +
+                    "\nDate/Time: " + moment(eventData[i].datetime).format('MMMM Do YYYY, h:mm a') + "\n",
+                    function (err) {
+                        if (err) throw err;
+                    });
             }
         })
 }
@@ -33,36 +40,37 @@ if (operator === "concert-this") {
 // * `spotify-this-song`
 // `node liri.js spotify-this-song '<song name here>'`
 if (operator === "spotify-this-song") {
-    let trackName = process.argv.slice(3).join(" ");
-    spotify.search({ type: 'track', query:'"'+trackName+'"'}, function(error, data){
-        if(!error){
-          for(var i = 0; i < data.tracks.items.length; i++){
-            var songData = data.tracks.items[i];
-            console.log(
-            "\nArtist: " + songData.artists[0].name +
-            "\nSong: " + songData.name +
-            "\nPreview URL: " + songData.preview_url +
-            "\nAlbum: " + songData.album.name
-            );
-   //adds text to log.txt
-//    fs.appendFile('log.txt', songData.artists[0].name);
-//    fs.appendFile('log.txt', songData.name);
-//    fs.appendFile('log.txt', songData.preview_url);
-//    fs.appendFile('log.txt', songData.album.name);
-//    fs.appendFile('log.txt', "-----------------------");
-          }      
-        } 
-        else{
-          console.log('Error occurred.');
+    // let trackName = process.argv.slice(3).join(" ");
+    spotify.search({ type: 'track', query: '"' + operator2 + '"' }, function (error, data) {
+        if (!error) {
+            var songData = data.tracks.items[0];
+            console.log
+                (
+                    "\nArtist: " + songData.artists[0].name +
+                    "\nSong: " + songData.name +
+                    "\nPreview URL: " + songData.preview_url +
+                    "\nAlbum: " + songData.album.name
+                );
+            fs.appendFile("log.txt",
+                "\nArtist: " + songData.artists[0].name +
+                "\nSong: " + songData.name +
+                "\nPreview URL: " + songData.preview_url +
+                "\nAlbum: " + songData.album.name + "\n",
+                function (err) {
+                    if (err) throw err;
+                });
         }
-      });
+        else {
+            console.log('Error occurred.');
+        }
+    });
 }
 
 // * `movie-this`
 // `node liri.js movie-this '<movie name here>'`
 if (operator === "movie-this") {
     axios
-        .get("http://www.omdbapi.com/?apikey=trilogy&t=" + operatorOMDB)
+        .get("http://www.omdbapi.com/?apikey=trilogy&t=" + operator2)
 
         .then(function (response) {
             var data = response.data;
@@ -75,15 +83,24 @@ if (operator === "movie-this") {
                 "\nPlot: " + data.Plot +
                 "\nActors: " + data.Actors
             )
+            fs.appendFile("log.txt", "\nTitle: " + data.Title +
+                "\nRelease Year: " + data.Year +
+                "\nIMDB Rating: " + data.imdbRating +
+                "\nRotten Tomatoes Rating: " + data.Ratings[1].Value +
+                "\nProduced In: " + data.Country +
+                "\nLanguage: " + data.Language +
+                "\nPlot: " + data.Plot +
+                "\nActors: " + data.Actors + "\n",
+                function (err) {
+                    if (err) throw err;
+                });
         }
         );
 }
 
 
 // * `do-what-it-says`
-// * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-// * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-// * Edit the text in random.txt to test out the feature for movie-this and concert-this.
+// other options to put into random.txt
 // spotify-this-song,"I Want it That Way"
 // movie-this,The Fifth Element
 // concert-this,The Killers
@@ -100,7 +117,59 @@ if (operator === "do-what-it-says") {
         let operator = dataArr[0];
         let operator2 = dataArr[1];
 
+        if (operator === "concert-this") {
+            axios
+                .get("https://rest.bandsintown.com/artists/" + operator2 + "/events?app_id=codingbootcamp")
+                .then(function (response) {
+                    eventData = response.data;
+                    for (var i = 0; i < eventData.length; i++) {
+                        console.log("\nBand/Artist: " + artistName +
+                            "\nVenue Name: " + eventData[i].venue.name +
+                            "\nVenue Location: " + eventData[i].venue.city + " " + eventData[i].venue.region + " " + eventData[i].venue.country +
+                            "\nDate/Time: " + moment(eventData[i].datetime).format('MMMM Do YYYY, h:mm a'));
+                        fs.appendFile("log.txt",
+                            "\nBand/Artist: " + artistName +
+                            "\nVenue Name: " + eventData[i].venue.name +
+                            "\nVenue Location: " + eventData[i].venue.city + " " + eventData[i].venue.region + " " + eventData[i].venue.country +
+                            "\nDate/Time: " + moment(eventData[i].datetime).format('MMMM Do YYYY, h:mm a') + "\n",
+                            function (err) {
+                                if (err) throw err;
+                            });
+                    }
+                })
+        }
 
+        // * `spotify-this-song`
+        // `node liri.js spotify-this-song '<song name here>'`
+        if (operator === "spotify-this-song") {
+            // let trackName = process.argv.slice(3).join(" ");
+            spotify.search({ type: 'track', query: '"' + operator2 + '"' }, function (error, data) {
+                if (!error) {
+                    var songData = data.tracks.items[0];
+                    console.log
+                        (
+                            "\nArtist: " + songData.artists[0].name +
+                            "\nSong: " + songData.name +
+                            "\nPreview URL: " + songData.preview_url +
+                            "\nAlbum: " + songData.album.name
+                        );
+                    fs.appendFile("log.txt",
+                        "\nArtist: " + songData.artists[0].name +
+                        "\nSong: " + songData.name +
+                        "\nPreview URL: " + songData.preview_url +
+                        "\nAlbum: " + songData.album.name + "\n",
+                        function (err) {
+                            if (err) throw err;
+                        });
+                }
+                else {
+                    console.log('Error occurred.');
+                }
+            });
+        }
+
+        // * `movie-this`
+        // `node liri.js movie-this '<movie name here>'`
         if (operator === "movie-this") {
             axios
                 .get("http://www.omdbapi.com/?apikey=trilogy&t=" + operator2)
@@ -116,25 +185,20 @@ if (operator === "do-what-it-says") {
                         "\nPlot: " + data.Plot +
                         "\nActors: " + data.Actors
                     )
+                    fs.appendFile("log.txt", "\nTitle: " + data.Title +
+                        "\nRelease Year: " + data.Year +
+                        "\nIMDB Rating: " + data.imdbRating +
+                        "\nRotten Tomatoes Rating: " + data.Ratings[1].Value +
+                        "\nProduced In: " + data.Country +
+                        "\nLanguage: " + data.Language +
+                        "\nPlot: " + data.Plot +
+                        "\nActors: " + data.Actors + "\n",
+                        function (err) {
+                            if (err) throw err;
+                        });
                 }
                 );
         }
-
-        if (operator === "concert-this") {
-            axios
-                .get("https://rest.bandsintown.com/artists/" + operator2 + "/events?app_id=codingbootcamp")
-                .then(function (response) {
-                    eventData = response.data;
-                    //   console.log (response);
-                    for (var i = 0; i < eventData.length; i++) {
-                        console.log("\nBand/Artist: " + operator2 +
-                            "\nVenue Name: " + eventData[i].venue.name +
-                            "\nVenue Location: " + eventData[i].venue.city + " " + eventData[i].venue.region + " " + eventData[i].venue.country +
-                            "\nDate/Time: " + moment(eventData[i].datetime).format('MMMM Do YYYY, h:mm a'));
-                    }
-                })
-        }
-
     });
 }
 
